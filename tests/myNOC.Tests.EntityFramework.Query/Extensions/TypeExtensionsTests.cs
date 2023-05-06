eb494fb2f5000bff33949d77b4605fbcc9100e1d
Eric Renken <eric@mynoc.com>
2023-05-06 16:54:01 -0400

    Adding unit tests

---
+++ tests/myNOC.Tests.EntityFramework.Query/Extensions/TypeExtensionsTests.cs
@@ -0,0 +1,12 @@
+namespace myNOC.Tests.EntityFramework.Query.Extensions
+{
+    [TestClass]
+    public class TypeExtensionTests 
+    {
+        [TestMethod]
+        public void CanImplement_Types_ImplementsInterface()
+        {
+            
+        }
+    }
+}
